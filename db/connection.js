151829f11b1614c73110cf8bad162115179b986b
Sandy McCabe <sandytech13@gmail.com>
2021-11-17 21:47:34 -0600

    back to SQL2

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,13 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 // Connect to database
 const db = mysql.createConnection({
-    host: 'localhost',
-    // Your MySQL username,
-    user: 'root',
-    // Your MySQL password
-    password: 'Bootcamp2021!',
-    database: 'employees'
-  });
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: 'Bootcamp2021!',
+  database: 'employees'
+});
 
-  module.exports = db;
+module.exports = db;
